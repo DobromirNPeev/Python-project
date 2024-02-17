914f48d1ca80718fcff9750c7080880e0235a06a
DobromirPeev <dobo22_2000@abv.bg>
2024-02-17 16:19:47 +0200

    fat

--- FifthRoundAddQuestion.py
+++ FifthRoundAddQuestion.py
@@ -14,9 +14,9 @@ class FifthRoundAddQuestion(AddQuestionMixin):
     def _create_interface(self):
         self.data={"question": "",
                    "answer(s)": []}
-        self.question_input=Button(screen_width//2-210,screen_height//2-60,200,50,"Question input:",lambda : None)
-        self.type_question=TextBoxForFiles(screen_width//2,screen_height//2-50,200,50,"question",self.data)
-        self.correct_answer=Button(screen_width//2-210,screen_height//2+30,200,50,"Correct answer:",lambda : None)
-        self.type_correct_answer=TextBoxForFiles(screen_width//2,screen_height//2+40,200,50,"answer(s)",self.data)
+        self.question_input=Button(screen_width//2-420,screen_height//2-60,200,50,"Question input:",lambda : None)
+        self.type_question=TextBoxForFiles(screen_width//2-210,screen_height//2-50,200,50,"question",self.data)
+        self.correct_answer=Button(screen_width//2-420,screen_height//2+30,200,50,"Correct answer:",lambda : None)
+        self.type_correct_answer=TextBoxForFiles(screen_width//2-210,screen_height//2+40,200,50,"answer(s)",self.data)
         self.done=Button(screen_width//2-100,screen_height//2+200,200,50,"Done",lambda : self._save_data())
         self.buttons=[self.question_input,self.type_question,self.correct_answer,self.type_correct_answer,self.done]
